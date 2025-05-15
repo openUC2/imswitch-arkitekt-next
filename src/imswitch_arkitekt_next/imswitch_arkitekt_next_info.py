435c5f68afcee97735c7f4daa70a11e905c4266d
beniroquai <bene.d@gmx.de>
2025-05-15 08:38:01 +0200

    updating to light-sheet plugin

--- src/imswitch_arkitekt_next/imswitch_arkitekt_next_info.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass(frozen=True)
-class  imswitch_arkitekt_next_info:
-    token: str
-
-
-
--- src/imswitch_arkitekt_next/imswitch_arkitekt_next_widget.py
+++
@@ -1,9 +0,0 @@
-from imswitch.imcontrol.view.widgets.basewidgets import Widget
-
-global_app = None
-class imswitch_arkitekt_next_widget(Widget):
-    """Linked to the arkitekt_next Controller ."""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
