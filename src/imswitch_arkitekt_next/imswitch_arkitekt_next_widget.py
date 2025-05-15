e39ce7e4ec23cc179550ccba28668001962a3fbc
beniroquai <bene.d@gmx.de>
2025-05-15 22:01:12 +0200

    Update imswitch_arkitekt_next_widget.py

--- src/imswitch_arkitekt_next/imswitch_arkitekt_next_widget.py
+++ src/imswitch_arkitekt_next/imswitch_arkitekt_next_widget.py
@@ -1,9 +1,15 @@
 from imswitch.imcontrol.view.widgets.basewidgets import Widget
 
-global_app = None
 class imswitch_arkitekt_next_widget(Widget):
     """Linked to the arkitekt_next Controller ."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
+class imswitch_arkitekt_next_widget_react(Widget):
+    """"""
+    pass
+
+class imswitch_arkitekt_next_ReactWidget(Widget):
+    """"""
+    print("react")
